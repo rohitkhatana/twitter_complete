a8ce59f42e369b2a20d96ca8fee0dd6d62cc3791
rohit khatana <rohitkhatana.khatana@gmail.com>
2013-12-06 23:48:24 +0530

    first version 1

---
+++ README.txt
@@ -0,0 +1,2 @@
+Twitter_complete is python module for interacting with the twitter apis.
+It is revised with latest twitter api versio 1.1
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+	name = 'twitter_complete',
+	packages = ['twitter_complete'],
+	version = '1.0',
+	description='',
+	author='rohit khatana',
+	author_email='rohitkhatana.khatana@gmail.com',
+     	url ='https://github.com/takirohit/twitter_complete',
+	classifiers = [
+	"Programming Language :: Python",
+	"Development Status :: 1 - Planning",
+	"License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+	"Environment :: Win32 (MS Windows)",
+	]
+)
